7388574cba84a43fbb933811dc87bb8a74947924
Chamika <chamikaw@gmail.com>
2026-02-16 00:28:26 +0000

    Add firebase integration
    Upgrade library versions

--- automotive/src/main/java/com/chamika/dashtune/DashTuneApplication.kt
+++ automotive/src/main/java/com/chamika/dashtune/DashTuneApplication.kt
@@ -1,7 +1,20 @@
 package com.chamika.dashtune
 
 import android.app.Application
+import com.google.firebase.Firebase
+import com.google.firebase.analytics.analytics
+import com.google.firebase.crashlytics.crashlytics
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class DashTuneApplication : Application()
+class DashTuneApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // Disable Firebase collection in debug builds
+        if (BuildConfig.DEBUG) {
+            Firebase.analytics.setAnalyticsCollectionEnabled(false)
+            Firebase.crashlytics.setCrashlyticsCollectionEnabled(false)
+        }
+    }
+}
